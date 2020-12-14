ad00858c44c6158710529d8c4b0a50aab1acf5ff
nDsCoop <68634774+nDsCoop@users.noreply.github.com>
2020-12-14 11:24:41 +0700

    Update contact.js

--- src/components/contact.js
+++ src/components/contact.js
@@ -40,7 +40,7 @@ class Contact extends Component {
 								<ListItem>
 									<ListItemContent style={{fontSize: '25px', fontFamilly:'Anton'}}>
 									<i className="fa fa-globe" aria-hidden='true'/>
-									 &#160; http://ndscoop.tech & https://ndsapp.herokuapp.com/
+									 &#160; We shall update soon
 									</ListItemContent>
 								</ListItem>
 							</List>
